0e5e8a60233628de11a1b687b149bad05ee9dfd9
deadr1pley <vanmatrix@hotmail.com>
2025-11-20 13:03:18 +0100

    uppdated kod

--- codestar/urls.py
+++ codestar/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from blog import views as blog_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')),
+    path('blog/', blog_views.my_blog, name="blog"), # the app urls are loaded as the main urls
 ]
