832fc0d48b020bc93baa49e2c280002c1e208737
deadr1pley <vanmatrix@hotmail.com>
2025-11-20 13:56:22 +0100

    added code for deplyment to heroku

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6shearj^2t1v#$fqksh)xycu0$(+r1)b+=m6z+yiq!bkv8$*13
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
